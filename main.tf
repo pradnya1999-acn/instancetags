949bea7aae36bbb04703a638911c972e8d1633e1
pradnya1999-acn <159138875+pradnya1999-acn@users.noreply.github.com>
2024-03-14 19:08:41 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "google" {
+#   version = "= 3.55.0"
+  credentials = var.sa_key
+  project = "parabolic-base-409505"
+}
+ 
+variable "sa_key" {
+  default =  ""
+}
+
+resource "google_compute_instance" "example_instance" {
+  name         = "example-instance"
+  machine_type = "n1-standard-1"
+  zone         = "your_zone"
+
+  tags = ["http-server", "https-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "your_image"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
